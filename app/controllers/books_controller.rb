1295e340cba5488891e57cacf8d2ac2f8c974b61
yo-p <papapapipipi42@gmail.com>
2020-08-27 06:12:12 +0000

    Bookの機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "You have creatad book successfully."
     else 
+      @books =Book.all
       render "index"
     end
   end
@@ -17,6 +18,27 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else  
+      render "edit"
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books
   end
 
   def edit
