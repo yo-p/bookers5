687cb2b6cbba11cacd0acf9557df5f57d64dfde2
yo-p <papapapipipi42@gmail.com>
2020-08-28 02:01:48 +0000

    URL直打ち　対策

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [ :edit, :update ] 
 
   def show
     @user = User.find(params[:id])
@@ -28,5 +29,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit( :name, :introduction, :profile_image )
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
