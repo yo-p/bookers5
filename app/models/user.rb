6862e55979bae9a7f9a7b782bfdeac41c712b06c
yo-p <papapapipipi42@gmail.com>
2020-08-30 05:33:23 +0000

    rspec追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :name, length: { in: 2..50 }
+  validates :name, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
   attachment :profile_image 
