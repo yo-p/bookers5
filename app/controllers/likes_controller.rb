b4c2042adfb94fce0d41e49441cea1237f0d6260
yo-p <papapapipipi42@gmail.com>
2020-09-11 01:41:59 +0000

    非同期でのいいね機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,11 @@ class LikesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @like = current_user.likes.create(book_id: params[:book_id])
-
-    # @book = Book.find(params[:book_id])
-    # @like = current_user.likes.create(book_id: params[:book_id])
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @like = Like.find_by(book_id: params[:book_id], user_id: current_user.id)
     @like.destroy
-
-    # @book = Book.find(params[:book_id])
-    # @like = Like.find_by(book_id: params[:book_id], user_id: current_user.id)
-    # @like.destroy
-    # redirect_back(fallback_location: root_path)
   end
 end
