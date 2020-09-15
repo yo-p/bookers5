f5b19c01125a2a0303881bc8c1d5b1c3a61b9cec
yo-p <papapapipipi42@gmail.com>
2020-09-15 08:25:37 +0000

    一覧画面にフォロー機能　追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,11 +4,11 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:follow_id])
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
+      flash[:notice] = 'ユーザーをフォローしました'
+      redirect_back(fallback_location: root_path)
     else 
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
+      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -16,11 +16,11 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:follow_id])
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
+      flash[:notice] = 'ユーザーのフォローを解除しました'
+      redirect_back(fallback_location: root_path)
     else  
       flash.now[:success] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
+      redirect_back(fallback_location: root_path)
     end
   end
 end
