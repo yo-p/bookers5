19095339346c916e648f3aff9fd49fd606b5e7d7
yo-p <papapapipipi42@gmail.com>
2020-09-15 05:56:33 +0000

    投稿一覧でのお気に入り　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :correct_user, only: [:destroy]
   
   def create
     @comment = current_user.comments.build(comment_params)
@@ -17,5 +18,12 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def correct_user
+    @comment = current_user.comments.find_by(id: params[:id])
+    unless @comment
+      redirect_to root_path
+    end
   end
 end
