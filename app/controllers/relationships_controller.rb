ee79d5302f13cf401edcd68b138153f282fbccda
yo-p <papapapipipi42@gmail.com>
2020-09-17 02:52:01 +0000

    フォロー機能を非同期

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,10 +5,10 @@ class RelationshipsController < ApplicationController
     following = current_user.follow(@user)
     if following.save
       flash[:notice] = 'ユーザーをフォローしました'
-      redirect_back(fallback_location: root_path)
+      # redirect_back(fallback_location: root_path)
     else 
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_back(fallback_location: root_path)
+      # redirect_back(fallback_location: root_path)
     end
   end
 
@@ -17,10 +17,10 @@ class RelationshipsController < ApplicationController
     following = current_user.unfollow(@user)
     if following.destroy
       flash[:notice] = 'ユーザーのフォローを解除しました'
-      redirect_back(fallback_location: root_path)
+      # redirect_back(fallback_location: root_path)
     else  
       flash.now[:success] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_back(fallback_location: root_path)
+      # redirect_back(fallback_location: root_path)
     end
   end
 end
